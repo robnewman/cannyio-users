6c0add3301f1a2b2be4b2cf21cf12e56d0728c44
Rob Newman <rob.newman@seqera.io>
2024-02-01 13:58:36 -0600

    Add Slackbot integration

--- return-users.py
+++ return-users.py
@@ -1,20 +1,32 @@
 import requests
 from dotenv import dotenv_values
+from slack_sdk import WebClient
 
 secrets = dotenv_values(".env")
 
 def loop_canny_users():
     increment = 100 # Canny API caps increment at 100
-    for i in range(0, 1000, increment):
-        print(f"Iterating through users {i}-{i+100}...")
+    missing_users = [] 
+    message ="Seqera Canny.io users with no Company affiliation:\n"
+    # Naively assume a max of 1000 users
+    for i in range(0, 1000, increment): 
         users = get_canny_users(secrets["API_KEY"], increment, i)
         if users:
-            print("Users with no Company affiliation:")
             for user in users:
                 if len(user['companies']) == 0:
-                    print(f"\t- User ID: {user['userID']}, Name: {user['name']}, Email: {user['email']}")
-        else:
-            print(f"\t- No users for users {i}-{i+100}.")
+                    missing_users.append(f"- Name: {user['name']}, Email: {user['email']}")
+
+    joined_users = '\n'.join(missing_users)
+
+    if len(missing_users) > 0:
+        client = WebClient(token=secrets["SLACKBOT_OAUTH_TOKEN"])
+        client.chat_postMessage(
+            channel = "proj-canny-launch",
+            text = message + joined_users,
+            username = "Bot User"
+        )
+    else:
+        print("No Seqera Canny users missing a Company affiliation")
 
 def get_canny_users(api_key, limit, skip):
     payload = {
